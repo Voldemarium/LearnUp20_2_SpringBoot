5f17ff497d5ff1230e1d4ddf0662204e20ae9ea6
vladimir <vladimir197717@mail.ru>
2023-05-26 13:49:15 +0400

    @Value in LogService

--- src/main/java/ru/learnUp/LearnUp20_2_SpringBoot/services/LogService.java
+++ src/main/java/ru/learnUp/LearnUp20_2_SpringBoot/services/LogService.java
@@ -1,16 +1,21 @@
 package ru.learnUp.LearnUp20_2_SpringBoot.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogService {
 	private final Logger logger;
 
+//	@Value("DEBUG")  //это "хардкод"
+	@Value("${log.level}")
+	private String level;
+
 	public LogService(Logger logger) {
 		this.logger = logger;
 	}
 
 	public void logMessage(String message) {
-		logger.log(message);
+		logger.log( "["+ level + "] " + message);
 	}
 }
